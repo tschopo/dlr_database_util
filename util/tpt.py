993422a1756a5575d8427e9d827fd4e01ae8f131
Johannes Polster <jopo@posteo.de>
2021-03-18 13:38:57 +0100

    Add all elevation output

--- util/tpt.py
+++ util/tpt.py
@@ -1,48 +1,60 @@
-from ElevationSampler import *
-from sqlalchemy import create_engine, text
+import re
 import numpy as np
 import pandas as pd
-import xlsxwriter
-import re
-from .sql import sql_get_osm, sql_get_trip_geom
+from sqlalchemy import text
 from .osm import get_osm_prop
+from .sql import sql_get_osm, sql_get_trip_geom
 
 
-def get_elevation_data(trip_id, osm_data, elevation_sampler, engine, first_sample_distance = 10, end_sample_distance = 100, brunnel_filter_length=10, interpolated = True, adjust_window_size = 12, std_thresh = 3, sub_factor = 3, clip = 20, smooth_window_size=301, poly_order=3, degrees=False, smooth_after_resampling=True, window_size_2=5,poly_order_2=1, mode="nearest"):
+def get_inclination(trip_id, osm_data, elevation_sampler, engine, first_sample_distance=10, end_sample_distance=100,
+                    brunnel_filter_length=10, interpolated=True, adjust_window_size=12, std_thresh=3, sub_factor=3,
+                    clip=20, smooth_window_size=301, poly_order=3, degrees=False, smooth_after_resampling=True,
+                    window_size_2=5, poly_order_2=1, mode="nearest", output_all=False):
     brunnels = get_osm_prop(osm_data, "brunnel", brunnel_filter_length=brunnel_filter_length)
-    
+
     trip_geom = sql_get_trip_geom(trip_id, engine, elevation_sampler.dem.crs)
-    
-    x_coords, y_coords, distances, elevation = elevation_sampler.elevation_profile(trip_geom, distance=first_sample_distance, interpolated = interpolated)
-    
+
+    x_coords, y_coords, distances, elevation = elevation_sampler.elevation_profile(trip_geom,
+                                                                                   distance=first_sample_distance,
+                                                                                   interpolated=interpolated)
+
     ele_brunnel = elevation_sampler.interpolate_brunnels(elevation, distances, brunnels, first_sample_distance)
 
-    ele_adjusted = elevation_sampler.adjust_forest_height(ele_brunnel,window_size = adjust_window_size, std_thresh = std_thresh, sub_factor = sub_factor, clip = clip)
-    
-    ele_smoothed =  elevation_sampler.smooth_ele(ele_adjusted, window_size=smooth_window_size, poly_order=poly_order, mode=mode)
+    ele_adjusted = elevation_sampler.adjust_forest_height(ele_brunnel, window_size=adjust_window_size,
+                                                          std_thresh=std_thresh, sub_factor=sub_factor, clip=clip)
 
-    distances_100, elevation_100 = elevation_sampler.resample_ele(ele_smoothed,distances,end_sample_distance)
+    ele_smoothed = elevation_sampler.smooth_ele(ele_adjusted, window_size=smooth_window_size, poly_order=poly_order,
+                                                mode=mode)
+
+    distances_100, elevation_100 = elevation_sampler.resample_ele(ele_smoothed, distances, end_sample_distance)
 
     if smooth_after_resampling:
-        elevation_100 = elevation_sampler.smooth_ele(elevation_100, window_size=window_size_2, poly_order=poly_order_2,mode=mode)
-
+        elevation_100 = elevation_sampler.smooth_ele(elevation_100, window_size=window_size_2, poly_order=poly_order_2,
+                                                     mode=mode)
 
     incl_100 = elevation_sampler.ele_to_incl(elevation_100, distances_100, degrees=degrees)
     distances_100 = distances_100[:-1]
-    
-    data = {"start_dist" : distances_100, "incl" : incl_100}
-    
+
+    if output_all:
+        return distances, elevation, ele_brunnel, ele_adjusted, ele_smoothed, distances_100, elevation_100
+
+    data = {"start_dist": distances_100, "incl": incl_100}
+
     return pd.DataFrame.from_dict(data)
 
-def get_timetable(trip_id, engine, min_stop_duration=30, round_int = True):
+
+def get_timetable(trip_id, engine, min_stop_duration=30, round_int=True):
     # distance from start 
     # station name
     # stop time at station in s
     # driving time to next station in s
 
-    sql = """
-    select geo_trips.trip_headsign, geo_stop_times.stop_sequence, geo_stop_times.arrival_time, geo_stop_times.departure_time, geo_stops.stop_name,
-    ST_LineLocatePoint(ST_Transform(geo_shape_geoms.geom,25832),ST_Transform(ST_SetSRID(ST_MakePoint(stop_lon,stop_lat),4326),25832)) * ST_length(ST_Transform(geo_shape_geoms.geom,25832)) as dist
+    sql = """\
+    select geo_trips.trip_headsign, geo_stop_times.stop_sequence,\
+    geo_stop_times.arrival_time, geo_stop_times.departure_time,\
+    geo_stops.stop_name, ST_LineLocatePoint(ST_Transform(geo_shape_geoms.geom,25832),\
+    ST_Transform(ST_SetSRID(ST_MakePoint(stop_lon,stop_lat),4326),25832))\
+     * ST_length(ST_Transform(geo_shape_geoms.geom,25832)) as dist
     from geo_stop_times, geo_stops, geo_trips, geo_shape_geoms
     where 
     geo_stops.stop_id = geo_stop_times.stop_id
@@ -51,34 +63,35 @@ def get_timetable(trip_id, engine, min_stop_duration=30, round_int = True):
     and geo_trips.trip_id = :trip_id
     order by stop_sequence, departure_time
     """
-    
-    time_table = pd.read_sql_query(text(sql), con=engine,params={"trip_id":trip_id})
 
-    s15 = pd.Timedelta(min_stop_duration*0.5, unit="s")
+    time_table = pd.read_sql_query(text(sql), con=engine, params={"trip_id": trip_id})
+
+    s15 = pd.Timedelta(min_stop_duration * 0.5, unit="s")
 
     last_stop = time_table.stop_sequence.iloc[-1]
     first_stop = time_table.stop_sequence.iloc[0]
 
     # ignore first and last station
     ign_frst_last = (time_table.stop_sequence > first_stop) & (time_table.stop_sequence < last_stop)
-    arr_eq_dep =  ign_frst_last & (time_table.departure_time == time_table.arrival_time)
+    arr_eq_dep = ign_frst_last & (time_table.departure_time == time_table.arrival_time)
 
     # ANNAHME: Züge halten mindesten 30s
     # if arrival time = departure time, then arrival time -15 and departure time + 15
-    time_table.loc[arr_eq_dep,["arrival_time"]] -= s15
-    time_table.loc[arr_eq_dep,["departure_time"]] += s15
+    time_table.loc[arr_eq_dep, ["arrival_time"]] -= s15
+    time_table.loc[arr_eq_dep, ["departure_time"]] += s15
 
     # stop duration = dperature - arrival
     time_table["stop_duration"] = (time_table.departure_time - time_table.arrival_time).dt.total_seconds()
 
     # driving time to next station:
     # take arrival time of next station and substract departure time 
-    driving_time = time_table.arrival_time[1:].dt.total_seconds().values-time_table.departure_time[:-1].dt.total_seconds().values
+    driving_time = time_table.arrival_time[1:].dt.total_seconds().values - time_table.departure_time[
+                                                                           :-1].dt.total_seconds().values
 
     driving_time = np.append(driving_time, 0)
 
     time_table["driving_time"] = driving_time
-    
+
     if round_int:
         time_table["dist"] = np.rint(time_table["dist"]).astype(int)
         time_table["stop_duration"] = np.rint(time_table["stop_duration"]).astype(int)
@@ -88,34 +101,33 @@ def get_timetable(trip_id, engine, min_stop_duration=30, round_int = True):
 
 
 def trip_title_to_filename(title):
-    special_char_map = {ord('ä'):'ae', ord('ü'):'ue', ord('ö'):'oe', ord('ß'):'ss', ord('('):'',  ord(')'):'',  ord(' '):'_'}
+    special_char_map = {ord('ä'): 'ae', ord('ü'): 'ue', ord('ö'): 'oe', ord('ß'): 'ss', ord('('): '', ord(')'): '',
+                        ord(' '): '_'}
 
     filename = title.lower()
-    filename = filename.replace(" - "," ")
+    filename = filename.replace(" - ", " ")
     filename = filename.translate(special_char_map)
     filename = re.sub('[^a-z0-9_]+', '', filename)
-    
+
     return filename
 
-def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclination):
-    
-    filename = trip_title_to_filename(trip_title)
-    
-    writer = pd.ExcelWriter(filename+'.xlsx', engine='xlsxwriter')
 
-    current_row = 0
+def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclination):
+    filename = trip_title_to_filename(trip_title)
+
+    writer = pd.ExcelWriter(filename + '.xlsx', engine='xlsxwriter')
 
     # we first need to write a dataframe, to get the sheets later
 
     # timetable
-    timetable.to_excel(writer, sheet_name='Timetable and limits', index=False,header=False,startrow=3)
+    timetable.to_excel(writer, sheet_name='Timetable and limits', index=False, header=False, startrow=3)
 
     workbook = writer.book
     worksheet = writer.sheets['Timetable and limits']
 
     # timetable end
-    current_row = timetable.shape[0]+3
-    worksheet.write(current_row,0,"]end")
+    current_row = timetable.shape[0] + 3
+    worksheet.write(current_row, 0, "]end")
     current_row += 1
 
     # 2 empty rows
@@ -137,12 +149,13 @@ def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclinat
     current_row += 1
 
     # electrification
-    electrification[["start_dist","electrified"]].to_excel(writer, sheet_name='Timetable and limits', index=False,header=False,startrow=current_row)
+    electrification[["start_dist", "electrified"]].to_excel(writer, sheet_name='Timetable and limits', index=False,
+                                                            header=False, startrow=current_row)
 
     current_row += electrification.shape[0]
 
     # ectrification end
-    worksheet.write(current_row,0,"]end")
+    worksheet.write(current_row, 0, "]end")
     current_row += 1
 
     # empty row
@@ -154,17 +167,17 @@ def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclinat
     current_row += 1
 
     # maxspeed
-    maxspeed[["start_dist","maxspeed"]].to_excel(writer, sheet_name='Timetable and limits', index=False,header=False,startrow=current_row)
+    maxspeed[["start_dist", "maxspeed"]].to_excel(writer, sheet_name='Timetable and limits', index=False, header=False,
+                                                  startrow=current_row)
 
     current_row += maxspeed.shape[0]
 
     # maxspeed end
-    worksheet.write(current_row,0,"]end")
+    worksheet.write(current_row, 0, "]end")
     current_row += 1
 
     # 2 empty rows
     current_row += 2
-
 
     worksheet.write(current_row, 0, "]accelerationLimits")
     worksheet.write(current_row, 1, "a [m/s^2]")
@@ -193,7 +206,7 @@ def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclinat
     worksheet.set_column(0, 3, 25)
 
     # new worksheet
-    inclination.to_excel(writer, sheet_name='Gradients and curves', index=False,header=False,startrow=3)
+    inclination.to_excel(writer, sheet_name='Gradients and curves', index=False, header=False, startrow=3)
 
     worksheet = writer.sheets['Gradients and curves']
 
@@ -206,18 +219,18 @@ def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclinat
     worksheet.write(2, 1, "Gradient [‰]")
 
     # inclination end
-    current_row = inclination.shape[0]+3
-    worksheet.write(current_row,0,"]end")
+    current_row = inclination.shape[0] + 3
+    worksheet.write(current_row, 0, "]end")
     current_row += 1
 
     # 2 empty rows
     current_row += 2
 
-    worksheet.write(current_row,0,"]curves")
-    worksheet.write(current_row,1,"Radius [m]")
+    worksheet.write(current_row, 0, "]curves")
+    worksheet.write(current_row, 1, "Radius [m]")
     current_row += 1
 
-    worksheet.write(current_row,0,"]end")
+    worksheet.write(current_row, 0, "]end")
 
     # column width
     worksheet.set_column(0, 3, 25)
@@ -226,7 +239,6 @@ def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclinat
 
     worksheet.write('A1', "]time_overhead")
     worksheet.write('B1', 0.05)
-
 
     worksheet.write('A4', "]gradient_interpolation_binary")
     worksheet.write('B4', 0)
@@ -243,8 +255,8 @@ def write_input_sheet(trip_title, timetable, electrification, maxspeed, inclinat
 
     worksheet.set_column(0, 3, 25)
 
-
     writer.save()
+
 
 def get_trip_data(trip_id, engine, elevation_sampler, crs=25832):
     """
@@ -252,17 +264,17 @@ def get_trip_data(trip_id, engine, elevation_sampler, crs=25832):
     -------
         title, timetable, electrification, maxspeed, inclination
     """
-    
-    osm_data = sql_get_osm(trip_id, engine, 25832)
-    
+
+    osm_data = sql_get_osm(trip_id, engine, crs)
+
     trip_headsign, timetable = get_timetable(trip_id, engine)
-    
+
     title = str(trip_id) + " " + trip_headsign
-    
+
     electrification = get_osm_prop(osm_data, "electrified")
 
     maxspeed = get_osm_prop(osm_data, "maxspeed")
 
-    inclination = get_elevation_data(trip_id, osm_data, elevation_sampler, engine)
+    inclination = get_inclination(trip_id, osm_data, elevation_sampler, engine)
 
     return title, timetable, electrification, maxspeed, inclination
